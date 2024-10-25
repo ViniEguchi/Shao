0cc7d9ccb9b0ac8dabae0de50e0fd654eb486f0d
ViniEguchi <vinicius.eguchi@sptech.school>
2024-10-24 22:35:27 -0300

    inicio dos inserts do banco de dados

--- Banco_Dados/Script.sql
+++ Banco_Dados/Script.sql
@@ -1,5 +1,27 @@
-drop database shao;
+DROP DATABASE shao;
 
-create database shao;
+CREATE DATABASE shao;
 
-use shao;
+USE shao;
+
+CREATE TABLE aluno (
+    idAluno INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    CPF CHAR(11) UNIQUE,
+    tel CHAR(11),
+    senha VARCHAR(45),
+    faixa VARCHAR(45),
+    fkMentor INT,
+    CONSTRAINT fkAlunoMentor FOREIGN KEY (fkMentor)
+        REFERENCES aluno (idAluno)
+);
+
+INSERT INTO aluno VALUES
+	(DEFAULT, '', '', '', '', '', ''),
+	(DEFAULT, '', '', '', '', '', ''),
+	(DEFAULT, '', '', '', '', '', ''),
+	(DEFAULT, '', '', '', '', '', ''),
+	(DEFAULT, '', '', '', '', '', ''),
+	(DEFAULT, '', '', '', '', '', ''),
+	(DEFAULT, '', '', '', '', '', ''),
+	(DEFAULT, '', '', '', '', '', '');
